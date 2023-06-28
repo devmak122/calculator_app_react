8ec5a9cb6c25fd8cd9b10122fb04a9301aba805d
devmak122 <118451518+devmak122@users.noreply.github.com>
2023-06-28 23:54:46 +0530

    Add files via upload

--- style.js
+++ style.js
@@ -1,10 +1,39 @@
-function insert(num) {
-        if ($(".display input").val() == 0) $(".display input").val("");
-        $(".display input").val($(".display input").val() + num);
+const inputBox=document.getElementById("input-box");
+const listcontainer = document.getElementById("list-container");
+
+function addtask(){
+    if(inputBox.value === ''){
+        alert("enter the task!!");
+    }
+        else{
+            let li=document.createElement("li");
+            li.innerHTML =inputBox.value;
+            listcontainer.appendChild(li);
+            let span = document.createElement("span");
+            span.innerHTML = "\u00d7";
+            li.appendChild(span);
         }
-function eql() {
-        $(".display input").val(eval($(".display input").val()));
+        inputBox.value = "";
+        savaData();
+
+    }
+    listcontainer.addEventListener("click" , function(e){
+        if(e.target.tagName === "LI"){
+            e.target.classList.toggle("checked");
+            savaData();
         }
-function cl() {
-        $(".display input").val(0);
+        else if(e.target.tagName === "span"){
+            e.target.parentElement.remove();
+            savaData();
+
         }
+    },false);
+
+    function savaData(){
+        localStorage.setItem("data",listContainer.innerHTML);
+
+    }
+    function showtask(){
+        listContainer.innerHTML = localStorage.getItem("data");
+    }
+    showtask();
